5edab14ce844261a24518e818b6c413c3a799c0c
Robert Höglund <robert.hoglund@bontouch.com>
2024-10-18 16:05:43 +0200

    ch1/1-1: Add support for request headers

--- src/browser/main.py
+++ src/browser/main.py
@@ -24,8 +24,17 @@ class URL:
     def __str__(self):
         return f"{self.scheme}://{self.host}{self.path}"
 
-    def request(self) -> str:
+    def request(self, headers=None) -> str:
         import socket
+
+        request_headers = {
+            "Host": self.host,
+            "Connection": "close",
+            "User-Agent": "browser.engineering/0.1",
+        }
+
+        if headers:
+            request_headers.update(headers)
 
         s = socket.socket(
             family=socket.AF_INET,
@@ -40,9 +49,12 @@ class URL:
             ctx = ssl.create_default_context()
             s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        request = f"GET {self.path} HTTP/1.0\r\n"
-        request += f"Host: {self.host}\r\n"
+        request = f"GET {self.path} HTTP/1.1\r\n"
+        for header, value in request_headers.items():
+            request += f"{header}: {value}\r\n"
         request += "\r\n"
+
+        print(request)
 
         s.send(request.encode("utf8"))
 
