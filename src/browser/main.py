c5fe154a29714f6e00d821826091a31f545255a8
Robert Höglund <robert.hoglund@bontouch.com>
2024-10-18 15:58:37 +0200

    ch1: Add support for custom port

--- src/browser/main.py
+++ src/browser/main.py
@@ -1,3 +1,6 @@
+from shutil import which
+
+
 class URL:
     def __init__(self, url: str):
         self.scheme, url = url.split("://", 1)
@@ -12,6 +15,10 @@ class URL:
             url += "/"
 
         self.host, url = url.split("/", 1)
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
         self.path = "/" + url
 
     def __str__(self):
