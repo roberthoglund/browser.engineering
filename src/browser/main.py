ae4d8108f9f737e26929716ea688b7dc65f5b5d0
Robert Höglund <robert.hoglund@bontouch.com>
2024-10-18 15:51:26 +0200

    ch1: Add rudimentary URL class with loader

---
+++ src/browser/main.py
@@ -0,0 +1,71 @@
+class URL:
+    def __init__(self, url: str):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http"
+
+        if "/" not in url:
+            url += "/"
+
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+    def __str__(self):
+        return f"{self.scheme}://{self.host}{self.path}"
+
+    def request(self) -> str:
+        import socket
+
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+
+        s.connect((self.host, 80))
+
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+
+        s.send(request.encode("utf8"))
+
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line in ("\n", "\r\n"):
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        content = response.read()
+        s.close()
+        return content
+
+
+def show(body: str):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+def load(url: URL):
+    body = url.request()
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+
+    load(URL(sys.argv[1]))
