480a22095b05a493084475972e22b9b08081cc6d
Robert Höglund <robert@roberthoglund.com>
2024-10-19 16:07:47 +0200

    ch2/2-1: Add paragraph on line break
    While this is not how HTML layouting works, it was the goal of the
    exercise.

--- src/browser/layout.py
+++ src/browser/layout.py
@@ -6,7 +6,10 @@ def layout(text: str, h_step: int, v_step: int, width: int):
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += h_step
 
-        if cursor_x >= width - h_step:
+        if c == "\n":
+            cursor_y += v_step + 8
+            cursor_x = h_step
+        elif cursor_x >= width - h_step:
             cursor_y += v_step
             cursor_x = h_step
     return display_list
